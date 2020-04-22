0105d882e295f283fddc994d7b838162d685949e
HIkeda <r31.tiuntiun.owata@gmail.com>
2020-04-22 23:26:05 +0900

    made batch file for win

---
+++ install_win.bat
@@ -0,0 +1,6 @@
+if not exist %HOMEDRIVE%%HOMEPATH%\_vimrc mklink %HOMEDRIVE%%HOMEPATH%\_vimrc %HOMEDRIVE%%HOMEPATH%\dotfiles\vimrc
+if not exist %HOMEDRIVE%%HOMEPATH%\_gvimrc mklink %HOMEDRIVE%%HOMEPATH%\_gvimrc %HOMEDRIVE%%HOMEPATH%\dotfiles\gvimrc
+if not exist %HOMEDRIVE%%HOMEPATH%\vimfiles\ mkdir %HOMEDRIVE%%HOMEPATH%\vimfiles
+if not exist %HOMEDRIVE%%HOMEPATH%\vimfiles\colors mklink /D %HOMEDRIVE%%HOMEPATH%\vimfiles\colors %HOMEDRIVE%%HOMEPATH%\dotfiles\colors
+
+pause
