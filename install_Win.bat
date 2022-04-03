33bd1cf50aed5f8caba8c0253c8e262ee4054a73
Hayato Ikeda <95620738+HayatoIkeda19890415@users.noreply.github.com>
2022-04-03 09:51:42 +0900

    can clone another directory

--- install_Win.bat
+++ install_Win.bat
@@ -1,5 +1,6 @@
-if not exist %HOMEDRIVE%%HOMEPATH%\_vimrc mklink %HOMEDRIVE%%HOMEPATH%\_vimrc %HOMEDRIVE%%HOMEPATH%\dotfiles\.vimrc
-if not exist %HOMEDRIVE%%HOMEPATH%\_gvimrc mklink %HOMEDRIVE%%HOMEPATH%\_gvimrc %HOMEDRIVE%%HOMEPATH%\dotfiles\.gvimrc
+set currentdir=%~dp0
+if not exist %HOMEDRIVE%%HOMEPATH%\_vimrc mklink %HOMEDRIVE%%HOMEPATH%\_vimrc %currentdir%\.vimrc
+if not exist %HOMEDRIVE%%HOMEPATH%\_gvimrc mklink %HOMEDRIVE%%HOMEPATH%\_gvimrc %currentdir%\.gvimrc
 if not exist %HOMEDRIVE%%HOMEPATH%\vimfiles\ mkdir %HOMEDRIVE%%HOMEPATH%\vimfiles
 
 pause
